796ce7bf8fd6509430b02bbe3c6d23091ca95496
Puja Agrawal <45425287+pujaagarwal5263@users.noreply.github.com>
2023-06-29 18:37:11 +0530

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/userdata",
+mongoose.connect("mongodb+srv://pujuagarwal5263:7590863884@cluster0.ca58dmt.mongodb.net/",
 {useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
