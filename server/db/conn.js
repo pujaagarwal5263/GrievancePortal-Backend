a32789b153da29464c34b2f5c1bde29cabe10c17
geekpuja <pujuagarwal5263@gmail.com>
2023-07-04 11:54:21 +0530

    changes

--- server/db/conn.js
+++ server/db/conn.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 
 //mongodb://localhost:27017/userdata
 
-mongoose.connect("mongodb://localhost:27017/grievance",
+mongoose.connect("mongodb+srv://pujuagarwal5263:7590863884@cluster0.ca58dmt.mongodb.net/",
 {useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
