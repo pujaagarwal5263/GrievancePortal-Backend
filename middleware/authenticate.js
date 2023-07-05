648563e29db47d39ac24fd8eff37555bb9b2aae3
Puja Agrawal <45425287+pujaagarwal5263@users.noreply.github.com>
2023-07-05 16:47:51 +0530

    Update authenticate.js

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -4,7 +4,7 @@ const User=require('../model/userSchema');
 const authenticate=async(req,res,next)=>{
     try{
       const token= req.cookies.jwtoken;
-      const verifyToken= jwt.verify(token,process.env.SECRET_KEY);
+      const verifyToken= jwt.verify(token,"thisisoursecretkey");
 
       const rootUser= await User.findOne({_id:verifyToken._id, "tokens.token":token});
 
