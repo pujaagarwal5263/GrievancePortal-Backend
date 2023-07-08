fe9ae0dcd2578fff143d1da14a6dd5d42b475084
geekpuja <pujuagarwal5263@gmail.com>
2023-07-08 12:22:19 +0530

    added two origins

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const Router=require('./router/auth');
 const app=express();
 // Enable CORS for all origins
 app.use(cors({
-    origin: 'https://grievance360.vercel.app', // Replace with the actual origin of your frontend application
+    origin: ['https://grievance360.vercel.app','http://localhost:3000'], // Replace with the actual origin of your frontend application
     credentials: true // Enable sending cookies with the request
   }));
 const cookieParser=require("cookie-parser");
